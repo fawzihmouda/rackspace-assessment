3ee16f630bf33d18f881a21cdc870d3e6f43f2fa
Fawzi <h.fawzi@hotmail.com>
2022-03-01 17:12:56 +1100

    Fix Lambda.tf

--- infrastructure/Lambda.tf
+++ infrastructure/Lambda.tf
@@ -1,8 +1,3 @@
-data "aws_s3_bucket" "selected" {
-  bucket = "terraform-backend-test-fawzi123"
-}
-
-
 resource "aws_lambda_function" "lambda_instance_refresh" {
   role          = aws_iam_role.lambda_iam_role.arn
   filename = "asg_instance_refresh.zip"
